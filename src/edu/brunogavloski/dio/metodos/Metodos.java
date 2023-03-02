d6f93be170689778a72f46dbf8843f42d0ad126f
Bruno Gavloski <bruno.gavloski@gmail.com>
2023-03-02 19:55:55 -0300

    metodos

---
+++ src/edu/brunogavloski/dio/metodos/Metodos.java
@@ -0,0 +1,10 @@
+package edu.brunogavloski.dio.metodos;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/edu/brunogavloski/dio/operadires/Operadores.java
@@ -0,0 +1,18 @@
+package edu.brunogavloski.dio.operadires;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int numero1 = 1;
+		int numero2 = 2;
+		
+		boolean simNao = numero1 != numero2;
+		
+		
+		System.out.println(simNao);
+	}
+
+}
